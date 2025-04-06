8f8bb88a2a91b1923d0d43bf10db9bc1ee607c22
Ramu8899ram <S3442260@live.tees.ac.uk>
2025-04-06 23:42:47 +0530

    Configured permissions for camera

--- app/src/main/java/uk/ac/tees/mad/expensetracker/screen/main/MainScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/expensetracker/screen/main/MainScreen.kt
@@ -1,5 +1,10 @@
 package uk.ac.tees.mad.expensetracker.screen.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -7,9 +12,12 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import uk.ac.tees.mad.expensetracker.component.DashboardTopAppbar
 import uk.ac.tees.mad.expensetracker.screen.dashboard.DashboardScreen
@@ -18,7 +26,25 @@ import uk.ac.tees.mad.expensetracker.screen.settings.SettingsScreen
 @Composable
 fun MainScreen(navController: NavController) {
     val selectedScreen = rememberSaveable { mutableIntStateOf(0) }
+    val context = LocalContext.current
 
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (!isGranted) {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
     Scaffold(
         topBar = {
             if (selectedScreen.intValue == 0) {
